31af04a35959b83145e9b3d10c91ac907d916b14
piedpiper-ff4084 <frfranco9217@gmail.com>
2021-03-15 16:13:17 -0700

    adding prod pipeline

--- lib/awsdevhour-backend-pipeline-stack.ts
+++ lib/awsdevhour-backend-pipeline-stack.ts
@@ -59,10 +59,10 @@ export class AwsdevhourBackendPipelineStack extends Stack {
     //Define application stage
     const devStage = pipeline.addApplicationStage(new AwsdevhourBackendPipelineStage(this, 'dev'));
 
-    // devStage.addActions(new ManualApprovalAction({
-    //   actionName: 'ManualApproval',
-    //   runOrder: devStage.nextSequentialRunOrder(),
-    // }));
+    devStage.addActions(new ManualApprovalAction({
+      actionName: 'ManualApproval',
+      runOrder: devStage.nextSequentialRunOrder(),
+    }));
 
     const prodStage = pipeline.addApplicationStage(new AwsdevhourBackendPipelineStage(this, 'prod'));
 
